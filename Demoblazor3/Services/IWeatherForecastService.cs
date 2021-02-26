9cf62e5cdf712c4ca7649f0a19f3ae02e220390c
Jose Eduardo Serrano <joser_isi@hotmail.com>
2021-02-26 16:25:19 -0600

    add project

---
+++ Demoblazor3/Services/IWeatherForecastService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SharedDemo;
+
+namespace Demoblazor3.Services
+{
+    public interface IWeatherForecastService
+    {
+        Task<WeatherForecast[]> GetAll();
+    }
+}
---
+++ Demoblazor3/Services/WeatherForecastService.cs
@@ -0,0 +1,23 @@
+﻿using SharedDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks; 
+
+namespace Demoblazor3.Services
+{
+    public class WeatherForecastService : IWeatherForecastService
+    {
+        private HttpClient Http;
+        public WeatherForecastService(HttpClient Http)
+        {
+            this.Http = Http;
+        }
+        async Task<WeatherForecast[]> IWeatherForecastService.GetAll()
+        {
+            return await Http.GetFromJsonAsync<WeatherForecast[]>("http://localhost:62700/WeatherForecast");
+        }
+    }
+}
